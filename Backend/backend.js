4f89a79640eb4ccc2c55474e32cafca4af45a4f9
Vaibhav Honakere <vhonaker@ucsc.edu>
2024-01-20 14:02:40 -0800

    Committed stuff

---
+++ Backend/backend.js
@@ -0,0 +1,41 @@
+import { db } from "./firebaseConfig";
+import { doc, setDoc, updateDoc, getDoc, getDocs } from 'firebase/firestore';
+
+export const getAllUserInfo = async() => {
+    const userCollection = collection(db, "users");
+    try{
+      console.log("First")
+      let data = await getDocs(userCollection);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data()
+      }))
+      console.log(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+}
+export const setUserInfo = async(userId, data) => {
+    console.log(data);
+    const workRef = doc(db, "users", userId);
+    await setDoc(workRef, data)
+        .then(() => {
+            console.log("successfully added user info.");
+        })
+        .catch((err) => {
+            console.log("error ", err);
+        })
+}
+
+// Creating a document for Fitness to specify routine
+// export async function createFitnessRoutine (userId, data) {
+//     console.log("The userId , ", userId)
+//     const workRef = doc(db, "Fitness", userId);
+//     await setDoc(workRef, data)
+//         .then(() => {
+//             console.log("successfully added user info.");
+//         })
+//         .catch((err) => {
+//             console.log("error ", err);
+//         })
+// }
+
--- Backend/firebaseConfig.js
+++ Backend/firebaseConfig.js
@@ -24,4 +24,3 @@ const analytics = getAnalytics(app);
 
 
 
-
